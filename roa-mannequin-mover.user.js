f7ab65c40a75a27b8cce577a7de9586f75df503d
Dave McClelland <davidmcclelland@gmail.com>
2018-03-27 21:07:55 -0400

    0.1.0: Attempting to persist loadouts across sessions

--- roa-mannequin-mover.user.js
+++ roa-mannequin-mover.user.js
@@ -7,7 +7,7 @@
 // @downloadURL    https://github.com/davidmcclelland/roa-mannequin-mover/raw/master/roa-mannequin-mover.user.js
 // @description    Moves the mannequin to a more convenient location
 // @match          https://*.avabur.com/game*
-// @version        0.0.4
+// @version        0.1.0
 // @run-at         document-end
 // @connect        githubusercontent.com
 // @connect        github.com
@@ -33,13 +33,13 @@
 	let checkForUpdateTimer = 0;
 
 	function checkForUpdate() {
-	    var version = "";
+	    let version = "";
 	    $.get(INTERNAL_UPDATE_URL).done(function(res){
-	        var match = atob(res.content).match(/\/\/\s+@version\s+([^\n]+)/);
+	        const match = atob(res.content).match(/\/\/\s+@version\s+([^\n]+)/);
             version = match[1];
 
 	        if (compareVersions(GM_info.script.version, version) < 0) {
-	            var message = "<li class=\"chat_notification\">RoA Mannequin Mover has been updated to version "+version+"! <a href=\"https://github.com/davidmcclelland/roa-mannequin-mover/raw/master/roa-mannequin-mover.user.js\" target=\"_blank\">Update</a> | <a href=\"https://github.com/davidmcclelland/roa-mannequin-mover/commits/master\" target=\"_blank\">Changelog</a></li>";
+	            const message = "<li class=\"chat_notification\">RoA Mannequin Mover has been updated to version "+version+"! <a href=\"https://github.com/davidmcclelland/roa-mannequin-mover/raw/master/roa-mannequin-mover.user.js\" target=\"_blank\">Update</a> | <a href=\"https://github.com/davidmcclelland/roa-mannequin-mover/commits/master\" target=\"_blank\">Changelog</a></li>";
 	            // TODO: Handle chat direction like ToA does
 	            $("#chatMessageList").prepend(message);
 	        } else {
@@ -48,6 +48,41 @@
 	    });
 	}
 
+	const AVAILABLE_LOADOUTS_KEY = 'RoaMMAvailableLoadouts';
+	const SELECTED_LOADOUT_KEY = 'RoaMMSelectedLoadout';
+
+	function saveMannequinState() {
+		const options = $('#mannequinOptions option');
+
+		let availableLoadouts = $.map(options, function(option) {
+			return option.value;
+		});
+
+		let selectedLoadout = $('#mannequinOptions').val();
+
+		localStorage.setItem(AVAILABLE_LOADOUTS_KEY, JSON.stringify(availableLoadouts));
+		localStorage.setItem(SELECTED_LOADOUT_KEY, selectedLoadout);
+	}
+
+	function loadMannequinState() {
+		let mannequinOptions = $('#mannequinOptions');
+		const options = JSON.parse(localStorage.getItem(AVAILABLE_LOADOUTS_KEY)) || [];
+		const selectedLoadout = localStorage.getItem(SELECTED_LOADOUT_KEY);
+
+		$.each(options, function(i, option) {
+			mannequinOptions.append($('<option>', {
+				value: option,
+				text: option
+			}));
+		});
+
+		if (selectedLoadout) {
+			$('#mannequinOptions').val(selectedLoadout);
+		}
+	}
+
+	loadMannequinState();
 	moveElements();
 	checkForUpdateTimer = setTimeout(checkForUpdate, 10000);
+	$('#mannequinSwap').click(saveMannequinState);
 })(jQuery);
